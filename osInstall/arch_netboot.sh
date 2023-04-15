8936981d1527114324fec886df4bb435ab14f562
b0r3df0x <129132351+b0r3df0x@users.noreply.github.com>
2023-04-15 23:25:13 +0200

    Redone arch_netboot
    Rough rewrite of the script in order to use systemd-boot and systemd-networkd

--- osInstall/arch_netboot.sh
+++ osInstall/arch_netboot.sh
@@ -1,41 +1,88 @@
 #!/bin/sh
+export LANG=en_US.UTF-8
 
-timezone="Region"/"Country"
+timezone=Europe/XXXX
+
+defdisk=/dev/sda
+
+user=XXXX
+hostname=XXXX
+
+echo -e "Arch Linux installer\nBy b0r3df0x\nFor a (soon to be) headless server.\n"
+
+read -p "What disk should I use? Type the path or press enter(Default: $defdisk):" disk
+disk=${disk:-/dev/sda}
+
+#echo $disk
 
 timedatectl set-ntp true
 timedatectl set-timezone $timezone
 
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+pacman -Sy
 
-pacman -Sy 
+parted -s $disk mklabel gpt
+parted -sa optimal $disk mkpart "EFI" fat32 0% 1.5GiB set 1 boot on
+parted -sa optimal $disk mkpart "SWAP" linux-swap 1.5GiB 10GiB
+parted -sa optimal $disk mkpart "ROOT" ext4 10GiB 100%
 
-efi=/dev/"sdXX"
-swap=/dev/"sdXX"
-root=/dev/"sdXX"
-#home=/dev/"sdXX"
-#tmp=/dev/"sdXX"
-#opt=/dev/"sdXX"
+mkfs.vfat -F32 ${disk}1
+mkswap ${disk}2
+mkfs.ext4 ${disk}3
 
-mkfs.vfat -F32 $efi
-mkswap $swap
-mkfs.ext4 $root
-#mkfs.ext4 $home
-#mkfs.ext4 $tmp
-#mkfs.ext4 $opt
+mount /mnt ${disk}3
+swapon ${disk}2
 
-mount $root /mnt
+pacstrap /mnt base base-devel linux linux-firmware linux-headers intel-ucode #amd-ucode if running on AMD cpu
 
-pacstrap /mnt base base-devel linux linux-firmware linux-headers intel-ucode #change to amd-ucode if running on amd cpu
-
-mkdir /mnt/boot/efi
-
-swapon $swap
-mount $home /mnt/home
-mount $efi /mnt/boot/efi
-#mount $tmp /mnt/tmp
-#mount $opt /mnt/opt
+mkdir /mnt/boot/EFI
+mount ${disk}1 /mnt/boot/EFI
 
 genfstab -U /mnt > /mnt/etc/fstab
 
-echo "Base installation completed. Proceed to chroot.\n\a"
+arch-chroot /mnt sh -c '
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen;
+echo "LANG=en_US.UTF-8" > /etc/locale.conf;
+echo "KEYMAP=us" > /etc/vconsle.conf;
+locale-gen;
+
+echo "$hostname" > /etc/hostname;
+
+#echo "127.0.0.1       localhost" > /etc/hosts;
+#echo "::1             localhost" >> /etc/hosts;
+#echo "127.0.1.1       $hostname.localdomain     $hostname" >> /etc/hosts;
+
+echo "[multilib]" >> /etc/pacman.conf;
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf;
+
+bootctl install --esp-path=/bin/EFI;
+
+pacman -Sy;
+
+pacman -S --noconfirm nano neovim trash-cli htop neofetch btop p7zip unrar zsh zsh-completions\
+ screen smartmontools pkgstats ufw clamav dmidecode gvfs gvfs-smb gvfs-mtp gpart cabextract ntfs-3g\
+ jfsutils f2fs-tools exfatprogs reiserfsprogs udftools nilfs-utils tar git go jdk11-openjdk jdk17-openjdk\
+ python-setuptools python-pip python-wheel nodejs php netbeans npm groovy nginx wpa_supplicant rsync samba wget net-tools\
+ dhclient bluez bluez-tools bluez-libs bluez-utils bluez-hid2hci bluez-plugins kismet\
+ libvirt virt-install qemu-full qemu-arch-extra ffmpeg;
+
+systemctl enable systemd-networkd;
+systemctl enable wpa_supplicant;
+
+useradd -m -G wheel,uucp,kismet,input,kvm,libvirt $user;
+
+echo "---Root Password---";
+passwd;
+
+echo "---User Password---";
+passwd $user;'
+
+clear
+
+sync
+umount -R /mnt/boot/EFI
+umount -R /mmnt
+swapoff ${disk}2
+
+echo -e "ALL DONE!\a"
